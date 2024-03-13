441847998b7575b5d90b93f35e204a4fb2ea0ad7
jason <jasongadewoltz@gmail.com>
2024-03-13 18:47:36 +0100

    png

--- ReadPNG.cpp
+++ ReadPNG.cpp
@@ -1,33 +1,20 @@
 #include "ReadPNG.h"
 
 #include <cassert>
+#include <cmath>
 #include <fstream>
 #include <iostream>
 
-const uint8_t ReadPNG::s_signature[8] = {137, 80, 78, 71, 13, 10, 26, 10};
-
-bool read(std::ifstream &ifs, Chunk &chunk)
-{
-    uint8_t temp[4];
-    ifs.read(reinterpret_cast<char *>(temp), 4);
-    chunk.length = get_uint32_t_from_data(temp);
-    ifs.read(reinterpret_cast<char *>(temp), 4);
-    chunk.name = get_chunk_name(temp);
-    chunk.data.resize(chunk.length);
-    ifs.read(reinterpret_cast<char *>(&chunk.data[0]), chunk.length);
-    ifs.read(reinterpret_cast<char *>(chunk.crc), 4);
-    return true;
-}
+const uint8_t ReadPNG::s_signature[8] = { 137, 80, 78, 71, 13, 10, 26, 10 };
 
 IHDR::IHDR()
-    : width(0), height(0), bit_depth(0), color_type(0), compression_method(0), filter_method(0), interlace_method(0)
-{
-}
+    : width(0), height(0), bit_depth(0), color_type(0), compression_method(0)
+    , filter_method(0), interlace_method(0) { }
 
-IHDR::IHDR(const Chunk &chunk)
+IHDR::IHDR(const Chunk& chunk)
 {
-    width = get_int32_t_from_data(chunk.data, 0);
-    height = get_int32_t_from_data(chunk.data, 4);
+    width = get_uint32_t_from_data(chunk.data, 0);
+    height = get_uint32_t_from_data(chunk.data, 4);
     bit_depth = chunk.data[8];
     color_type = chunk.data[9];
     compression_method = chunk.data[10];
@@ -35,7 +22,42 @@ IHDR::IHDR(const Chunk &chunk)
     interlace_method = chunk.data[12];
 }
 
-ReadPNG::ReadPNG(const char *path)
+Chunk::Chunk() : length(0), name{}, crc{} { }
+
+bool Chunk::check_crc(const uint32_t calculated_crc) const
+{
+    uint32_t crc_file = (crc[0] << 24) + (crc[1] << 16) + (crc[2] << 8) + crc[3];
+    return crc_file == calculated_crc;
+}
+
+void Chunk::create_crc_table()
+{
+    for (int32_t n = 0; n < 256; n++) {
+        auto c = static_cast<uint32_t>(n);
+        for (int32_t k = 0; k < 8; k++) {
+            if (c & 1)
+                c = 0xedb88320L ^ (c >> 1);
+            else
+                c = c >> 1;
+        }
+        crc_table[n] = c;
+    }
+}
+
+unsigned long Chunk::update_crc(const uint32_t crc, const std::vector<uint8_t>& buffer)
+{
+    unsigned long c = crc;
+    for (const uint8_t buf : buffer)
+        c = crc_table[(c ^ buf) & 0xff] ^ (c >> 8);
+    return c;
+}
+
+unsigned long Chunk::get_crc(const std::vector<uint8_t>& buffer)
+{
+    return update_crc(0xffffffffL, buffer) ^ 0xffffffffL;
+}
+
+ReadPNG::ReadPNG(const char* path)
 {
     std::ifstream ifs;
     ifs.open(path, std::ios::in | std::ios::binary);
@@ -46,37 +68,29 @@ ReadPNG::ReadPNG(const char *path)
     this->c_path = path;
     if (!read_signature(ifs))
         throw std::exception();
+    Chunk::create_crc_table();
+    std::vector<uint8_t> data;
     Chunk chunk;
-    read(ifs, chunk);
+    read_chunk(ifs, chunk);
     m_chunks.push_back(chunk);
-    if (chunk.name == "IDHR" || chunk.length != 13) {
+    if (chunk.name != "IHDR" || chunk.length != 13) {
         std::cout << "Faulty starting chunk";
         throw std::exception();
     }
     m_header = IHDR(chunk);
-    if (m_header.bit_depth != 8 ||
-        m_header.color_type != 6 ||
-        m_header.compression_method != 0 ||
-        m_header.filter_method != 0 ||
-        m_header.interlace_method != 0) {
-        std::cout << "unsupported png type";
-        throw std::exception();
-    }
     while (chunk.name != "IEND") {
-        read(ifs, chunk);
+        read_chunk(ifs, chunk);
         m_chunks.push_back(chunk);
     }
-
-    for (const Chunk& chunk_temp : m_chunks)
-        if (chunk_temp.name == "IDAT")
-            m_data.insert(m_data.end(), chunk_temp.data.begin(), chunk_temp.data.end());
+    std::vector<uint8_t> compressed_data = get_idat_data();
+    std::cout << "done";
 }
 
-bool ReadPNG::read_signature(std::ifstream &ifs)
+bool ReadPNG::read_signature(std::ifstream& ifs)
 {
     constexpr uint8_t length_signature = 8;
     uint8_t signature[length_signature];
-    ifs.read(reinterpret_cast<char *>(signature), length_signature);
+    ifs.read(reinterpret_cast<char*>(signature), length_signature);
     for (int32_t i = 0; i < length_signature; ++i)
         if (signature[i] != s_signature[i]) {
             std::cout << "Faulty signature at byte " << i + 1 << '\n';
@@ -85,23 +99,65 @@ bool ReadPNG::read_signature(std::ifstream &ifs)
     return true;
 }
 
-Chunk::Chunk(): length(0), name{}, crc{} { }
-
-int32_t get_int32_t_from_data(const std::vector<uint8_t> &data, const int32_t start)
+bool ReadPNG::read_chunk(std::ifstream& ifs, Chunk& chunk)
 {
-    assert(start + 4 < data.size() && "faulty length for uint32_t read");
-    uint8_t temp[4];
-    for (int32_t i = 0; i < 3; ++i)
-        temp[i] = data[i + start];
-    return temp[3] + (temp[2] << 8) + (temp[1] << 16) + (temp[0] << 24);
+    std::vector<uint8_t> temp(4);
+    ifs.read(reinterpret_cast<char*>(&temp[0]), 4);
+    chunk.length = get_uint32_t_from_data(temp, 0);
+    std::vector<uint8_t> buffer(chunk.length + 4);
+    ifs.read(reinterpret_cast<char*>(&buffer[0]), chunk.length + 4);
+    ifs.read(reinterpret_cast<char*>(&chunk.crc[0]), 4);
+    if (!chunk.check_crc(Chunk::get_crc(buffer))) {
+        std::cout << "Faulty crc code";
+        throw std::exception();
+    }
+    for (int32_t i = 0; i < 4; ++i)
+        temp[i] = buffer[i];
+    chunk.name = get_chunk_name(temp);
+    chunk.data.resize(chunk.length);
+    for (int32_t i = 0; i < chunk.length; ++i)
+        chunk.data[i] = buffer[i + 4];
+    return true;
 }
 
-static uint32_t get_uint32_t_from_data(const uint8_t *data)
+int32_t ReadPNG::get_idat_data_length()
 {
-    return data[3] + (data[2] << 8) + (data[1] << 16) + (data[0] << 24);
+    int32_t length = 0;
+    for (const Chunk& chunk : m_chunks)
+        if (chunk.name == "IDAT")
+            length += chunk.length;
+    return length;
 }
 
-std::ostream &operator<<(std::ostream &os, const Chunk &chunk)
+std::vector<uint8_t> ReadPNG::get_idat_data()
+{
+    std::vector<uint8_t> result(get_idat_data_length());
+    int32_t index = 0;
+    for (const Chunk& chunk : m_chunks)
+        if (chunk.name == "IDAT")
+            for (const uint8_t data : chunk.data) {
+                result[index] = data;
+                ++index;
+            }
+    return result;
+}
+
+std::vector<uint8_t> ReadPNG::deflate(const std::vector<uint8_t>& compressed_data) const
+{
+    std::vector<uint8_t> picture(m_header.height * m_header.width * 3 + m_header.height);
+
+    return picture;
+}
+
+static uint32_t get_uint32_t_from_data(const std::vector<uint8_t>& data, int32_t posistion)
+{
+    uint32_t result = 0;
+    for (uint32_t i = 0; i < 4; ++i)
+        result += data[i + posistion] * std::pow(256, 3 - i);
+    return result;
+}
+
+std::ostream& operator<<(std::ostream& os, const Chunk& chunk)
 {
     os << "Length " << chunk.length << '\n';
     os << "Name " << chunk.name << '\n';
@@ -110,7 +166,7 @@ std::ostream &operator<<(std::ostream &os, const Chunk &chunk)
     return os;
 }
 
-std::ostream &operator<<(std::ostream &os, const ReadPNG &read_png)
+std::ostream& operator<<(std::ostream& os, const ReadPNG& read_png)
 {
     os << "File name " << read_png.path() << '\n';
     os << "Heigth " << read_png.height() << '\n';
@@ -118,7 +174,7 @@ std::ostream &operator<<(std::ostream &os, const ReadPNG &read_png)
     return os;
 }
 
-std::ostream & operator<<(std::ostream &os, const IHDR &header)
+std::ostream& operator<<(std::ostream& os, const IHDR& header)
 {
     os << "Height " << header.height << '\n';
     os << "Width " << header.width << '\n';
@@ -130,16 +186,16 @@ std::ostream & operator<<(std::ostream &os, const IHDR &header)
     return os;
 }
 
-std::string get_chunk_name(const uint8_t *na)
+std::string get_chunk_name(const std::vector<uint8_t>& data)
 {
     std::string result;
     for (uint8_t i = 0; i < 4; ++i) {
-        if (!isalpha(na[i]))
+        if (!isalpha(data[i]))
             throw std::exception();
     }
-    result += static_cast<char>(na[0]);
-    result += static_cast<char>(na[1]);
-    result += static_cast<char>(na[2]);
-    result += static_cast<char>(na[3]);
+    result += static_cast<char>(data[0]);
+    result += static_cast<char>(data[1]);
+    result += static_cast<char>(data[2]);
+    result += static_cast<char>(data[3]);
     return result;
 }
--- ReadPNG.h
+++ ReadPNG.h
@@ -7,12 +7,18 @@
 #include <ostream>
 #include <vector>
 
+static unsigned long crc_table[256];
+
 struct Chunk {
-    uint32_t length;
+    uint64_t length;
     std::string name;
     std::vector<uint8_t> data;
     uint8_t crc[4];
     Chunk();
+    bool check_crc(uint32_t calculated_crc) const;
+    static unsigned long update_crc(uint32_t crc, const std::vector<uint8_t>& buffer);
+    static unsigned long get_crc(const std::vector<uint8_t>& buffer);
+    static void create_crc_table();
 };
 
 struct IHDR {
@@ -30,28 +36,28 @@ struct IHDR {
 
 class ReadPNG {
     IHDR m_header;
-    //int32_t m_offset;
     static const uint8_t s_signature[8];
     const char* c_path;
     std::vector<Chunk> m_chunks;
-    std::vector<uint8_t> m_data;
 public:
-    explicit ReadPNG(const char *path);
+    explicit ReadPNG(const char* path);
 
     // ACCESSORS
-    [[nodiscard]] const char* path() const {return c_path;}
-    [[nodiscard]] int32_t width() const {return m_header.width;}
-    [[nodiscard]] int32_t height() const {return  m_header.height;}
+    [[nodiscard]] const char* path() const { return c_path; }
+    [[nodiscard]] int32_t width() const { return m_header.width; }
+    [[nodiscard]] int32_t height() const { return  m_header.height; }
 private:
     static bool read_signature(std::ifstream& ifs);
+    std::vector<uint8_t> get_idat_data();
+    int32_t get_idat_data_length();
+    static bool read_chunk(std::ifstream& ifs, Chunk& chunk);
+    [[nodiscard]] std::vector<uint8_t> deflate(const std::vector<uint8_t>& compressed_data) const;
 };
 
-static int32_t get_int32_t_from_data(const std::vector<uint8_t>& data, int32_t start);
-static uint32_t get_uint32_t_from_data(const uint8_t *data);
+static uint32_t get_uint32_t_from_data(const std::vector<uint8_t>& data, int32_t posistion);
 std::ostream& operator<<(std::ostream& os, const Chunk& chunk);
 std::ostream& operator<<(std::ostream& os, const ReadPNG& read_png);
 std::ostream& operator<<(std::ostream& os, const IHDR& header);
-bool read(const char *path, std::ifstream &ifs, Chunk& chunk);
-std::string get_chunk_name(const uint8_t *na);
+std::string get_chunk_name(const std::vector<uint8_t>& data);
 
 #endif //READ_PNG_H
